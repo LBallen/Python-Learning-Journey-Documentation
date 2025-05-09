276c472f9e09fd81259a3d4b1692c25272c732fc
LBallen <lnrdballen@gmail.com>
2025-05-08 22:15:59 -0400

    Finshed Day one!

---
+++ Week_1_Basics/Examples/Variables and Types.py
@@ -0,0 +1,12 @@
+age = 26
+name = "leo"
+height = 5.8
+print("age = ", age)
+print("Name:", name)
+print("Height:", height)
+
+x, y, z = 1,2,3
+x,y = y,x
+print("x", x)
+print("y", y)
+print("z", z)
